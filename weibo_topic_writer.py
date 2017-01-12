ec7815789a44ba4bbfb31af5773690ff9afa87f6
binking <jiangzhibin2014.xujie@gmail.com>
2017-01-12 20:07:53 +0800

    debug

--- weibo_topic_writer.py
+++ weibo_topic_writer.py
@@ -74,7 +74,7 @@ class WeiboTopicWriter(DBAccesor):
         select_topic_sql = """
             SELECT DISTINCT topic_url FROM topicinfo 
             -- WHERE  theme LIKE '新浪微博_热门话题%' 
-            WHERE createdate > date_sub(NOW(), INTERVAL 30 DAY )
+            WHERE createdate > date_sub(NOW(), INTERVAL 5 DAY )
             -- Update 30 days' Topics Detail pre day
         """
         conn = self.connect_database()
