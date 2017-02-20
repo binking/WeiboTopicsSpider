60884f261dd6ebc9a964bd714755766fe7d47d15
binking <jiangzhibin2014.xujie@gmail.com>
2017-02-20 11:00:33 +0800

    update topicinfo

--- weibo_topic_writer.py
+++ weibo_topic_writer.py
@@ -29,10 +29,19 @@ class WeiboTopicWriter(DBAccesor):
             SELECT * FROM WeiboTrend 
             WHERE topic_url = %s AND crawl_dt = %s)
         """
-        update_info_sql = """
+        update_info_sql_1 = """
             UPDATE weibotopic 
-            set title=%s, introduction=%s, read_num=%s, read_num_dec=%s, discussion_num=%s, fans_num=%s, topic_type=%s, topic_region=%s, label=%s, topic_url=%s, logo_img_url=%s
+            set title=%s, introduction=%s, read_num=%s, read_num_dec=%s, 
+            discussion_num=%s, fans_num=%s, topic_type=%s, topic_region=%s, 
+            label=%s, topic_url=%s, logo_img_url=%s
             WHERE topic_url=%s
+        """
+        Topicinfo(createdate, title, logo_img_url, label, introduction, topic_type, 
+keywords, event_class)
+        update_info_sql_2 = """
+            UPDATE Topicinfo 
+            set introduction=%s, topic_type=%s, topic_region=%s, label=%s, logo_img_url=%s
+            WHERE title=%s
         """
         conn = self.connect_database()
         cursor = conn.cursor()
@@ -46,7 +55,7 @@ class WeiboTopicWriter(DBAccesor):
                 info.get('image_url', ''), info['topic_url'],
                 info['access_time']
             ))
-            cursor.execute(update_info_sql, (
+            cursor.execute(update_info_sql_1, (
                 info['title'], info.get('guide', ''),
                 info.get('read_num', ''), info.get('read_num_dec', 0),
                 info.get('dis_num', ''), info.get('fans_num', ''),
@@ -54,7 +63,12 @@ class WeiboTopicWriter(DBAccesor):
                 info.get('label', ''), info['topic_url'],
                 info['image_url'], info['topic_url']
             ))
-            print 'Writed topic and trend DONE(%s)!!!' % info['topic_url']
+            cursor.execute(update_info_sql_2, (
+                info.get('guide', ''), info.get('type', ''), 
+                info.get('region', ''), info.get('label', ''),
+                info['image_url'], info['title']
+            ))
+            print 'Writed topic and trend DONE(%s)!!!' % info['title']
             conn.commit(); cursor.close(); conn.close()
         except Exception as e:
             traceback.print_exc()
